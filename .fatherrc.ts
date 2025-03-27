554fba0e77225b631eb2ded533ab67d214fea478
baic <303394539@qq.com>
2025-03-27 17:14:57 +0800

    优化编译

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,3 +1,21 @@
 import { defineConfig } from '@baic/father-plugin-yolk';
 
-export default defineConfig();
+export default defineConfig({
+  platform: 'browser',
+  esm: {
+    overrides: {
+      'src/code-editor-webpack-plugin': {
+        platform: 'node',
+        output: 'es/code-editor-webpack-plugin',
+      },
+    },
+  },
+  cjs: {
+    overrides: {
+      'src/code-editor-webpack-plugin': {
+        platform: 'node',
+        output: 'lib/code-editor-webpack-plugin',
+      },
+    },
+  },
+});
